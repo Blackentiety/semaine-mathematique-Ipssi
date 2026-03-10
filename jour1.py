6912f01fc050bf48d9ef3a8661e7a26ab067bd8b
lukas <lukas.boudinl@gmail.com>
2026-03-10 14:29:03 +0100

    ajout fonction regression

--- jour1.py
+++ jour1.py
@@ -3,6 +3,9 @@ v2 = [1,2]
 v3 = [5,9]
 m1 = [[1,2],[2,4]]
 
+surface = [50,70,90]
+prix = [100, 140, 180]
+
 def v_add(v1,v2):
     res = [v1[i] + v2[i] for i in range(len(v1))]
     return res 
@@ -30,4 +33,44 @@ def mv_produit(matrice,vecteur):
 
 print(mv_produit(m1,v1))
 
+def moyenne(liste):
+    resultat = 0
+    for valeur in liste:
+        resultat+= valeur
+    return resultat/len(liste)
+
+moyenne_surface = moyenne(surface)
+moyenne_prix = moyenne(prix)
+
+def numerateur(liste):
+    m = moyenne(liste)
+    resultat = []
+    for valeur in liste:
+        resultat.append((valeur-m))
+    return resultat
+
+num_a = numerateur(surface)
+num_b = numerateur(prix)
+
+def calcul_final_a(vecteur1, vecteur2):
+    num_a = numerateur(vecteur1)
+    print(num_a)
+    num_b = numerateur(vecteur2)
+    print(num_b)
+    denominateur = v_multi(num_a,num_a)
+    print(denominateur)
+    print(v_multi(num_a,num_b))
+    resultat = v_multi(num_a,num_b)/denominateur
+    return resultat
+
+print(calcul_final_a(surface,prix))
+
+def calcul_final_b(vecteur1, vecteur2):
+    a = calcul_final_a(vecteur1, vecteur2)
+    moy_x = moyenne(surface)
+    moy_y = moyenne(prix)
+    b = moy_y - (a * moy_x)
+    return b 
+
+print(calcul_final_b(prix, surface))
 
